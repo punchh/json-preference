ca263fa96ee28b3fa0b6491882e1c4b48dd04083
Tai Do <tai.do@partech.com>
2023-12-28 13:06:14 -0500

    Fix Rails migration

--- lib/json-preference/serializer.rb
+++ lib/json-preference/serializer.rb
@@ -5,6 +5,8 @@ module JsonPreference
     end
 
     def self.load(hash)
+      return hash if hash.is_a?(String) # For Rails migration only. Please remove this line after it
+
       (hash || {}).with_indifferent_access
     end
   end
